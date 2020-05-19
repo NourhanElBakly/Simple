02db4ab074c9582978651cdc23acf623b2f5eb3a
Nourhan ElBakly <nelbakly@banquemisr.com>
2020-05-19 13:59:27 +0200

    Added add function

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,8 @@
 fun main(args: Array<String>){
-	print("Hello, World!")
+	print("Hello, QUARANTIED World!")
+	print("total = " + add(1, 2))
+}
+
+fun add(num1: Int, num2: Int): Int {
+	return num1 + num2
 }
